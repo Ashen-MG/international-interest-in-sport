ae592522d9b0e7d2a4ea191e8c7a5d02bc9ed8bf
Sabinka <ssamporova@gmail.com>
2022-04-02 17:07:01 +0200

    prepare frontend formular for adding new user

--- frontend/src/admin/pages/account_management/AccountManagement.tsx
+++ frontend/src/admin/pages/account_management/AccountManagement.tsx
@@ -3,7 +3,7 @@ import {Dropzone, dropzoneFileProp} from "components/drag_and_drop/Dropzone";
 import React, {useEffect, useState} from "react";
 import createSnackbar, {SnackTypes} from "../../../components/snackbar/Snackbar";
 import {useCountries, useMutationWithNotifications} from "../../../app/hooks";
-import {apiUploadFiles} from "../../adapters";
+import {apiAddNewCountry, apiUploadFiles} from "../../adapters";
 import {currencies} from "../../../data/active_currency_codes";
 import {Button, Col, FloatingLabel, Form, Row} from "react-bootstrap";
 import Select from "react-select";
@@ -19,9 +19,93 @@ import config from "../../../config";
  * */
 export const AccountManagement = () => {
 
+    const [newUserEmail, setnewUserEmail] = useState<string>("");
+    const [newUserPassword, setNewUserPassword] = useState<string>("");
+    const [newUserType, setNewUserType] = useState<string>("");
+
+    //const addNewCountryMutation = useMutationWithNotifications(
+    //    "adding_new_country", apiAddNewCountry, "Adding new country...", "en"
+    //);
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (newUserEmail.length === 0 || newUserPassword.length === 0 || newUserType.length === 0) {
+            createSnackbar("All fields are required.", SnackTypes.warn); return;
+        }
+        //addNewCountryMutation.mutate({email: newUserEmail, password: newUserPassword, type: newUserType});
+        console.log({email: newUserEmail, password: newUserPassword, type: newUserType});
+    }
+
     return(<>
         <CenteredRow as="header" lg={6} md={7}>
             <h1>Account Management</h1>
+        </CenteredRow>
+
+        <CenteredRow as="section" className="mb-3" lg={6} md={7}>
+            <h2>Add new user account</h2>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group as={Row} className="mb-4" controlId="formNewUserEmail">
+                    <Col>
+                        <FloatingLabel controlId="floatingNewUserEmail" label="User email">
+                            <Form.Control type="text"
+                                          placeholder="User email"
+                                          value={newUserEmail}
+                                          onChange={(e) =>
+                                              setnewUserEmail((e.currentTarget as HTMLInputElement).value)}
+                            />
+                        </FloatingLabel>
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} className="mb-4" controlId="formNewUserPassword">
+                    <Col>
+                        <FloatingLabel controlId="floatingNewUserPassword" label="New User Password">
+                            <Form.Control type="text"
+                                          placeholder="New User Password"
+                                          value={newUserPassword}
+                                          onChange={(e) =>
+                                              setNewUserPassword((e.currentTarget as HTMLInputElement).value)}
+                            />
+                        </FloatingLabel>
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} className="mb-4" controlId="formNewUserType">
+                    <Col>
+                        <div>
+                            <label>
+                                <input
+                                    type = "radio"
+                                    value = "admin"
+                                    checked = {newUserType === "admin"}
+                                    onChange = {(e) =>
+                                        setNewUserType((e.currentTarget as HTMLInputElement).value)}
+                                />
+                            Admin
+                            </label>
+                        </div>
+                        <div>
+                            <label>
+                                <input
+                                    type = "radio"
+                                    value = "secretary"
+                                    checked = {newUserType === "secretary"}
+                                    onChange = {(e) =>
+                                        setNewUserType((e.currentTarget as HTMLInputElement).value)}
+                                />
+                            Secretary
+                            </label>
+                        </div>
+
+                    </Col>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Add user
+                </Button>
+            </Form>
+        </CenteredRow>
+
+        <CenteredRow>
+        <h2>Update user account</h2>
+        <p>TODO</p>
         </CenteredRow>
 
     </>)
